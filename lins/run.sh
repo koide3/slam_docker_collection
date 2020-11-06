da06a316b836e754fd7aa3085f9e53e1e939013c
k.koide <k.koide@aist.go.jp>
2020-11-06 14:28:06 +0900

    remove lins

--- lins/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo docker run --net=host -it --rm \
-                -v $(realpath LINS---LiDAR-inertial-SLAM):/root/catkin_ws/src/LINS---LiDAR-inertial-SLAM \
-                -w /root/catkin_ws/src/LINS---LiDAR-inertial-SLAM \
-                $@ \
-                lins
