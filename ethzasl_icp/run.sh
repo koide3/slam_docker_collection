987cfd1dab95e1bda18fa340007e6b7f5c087950
k.koide <k.koide@aist.go.jp>
2020-11-06 12:02:47 +0900

    remove ethasl_icp at a moment

--- ethzasl_icp/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo docker run --net host -it --rm \
-                -w /root/catkin_ws2/src/ethzasl_icp_mapping \
-                -v $(realpath ethzasl_icp_mapping):/root/catkin_ws2/src/ethzasl_icp_mapping \
-				$@ \
-				ethzasl_icp
--- hdl_graph_slam/build.sh
+++ hdl_graph_slam/build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo docker build --tag hdl_graph_slam -f Dockerfile .
-
+docker build --tag hdl_graph_slam -f Dockerfile .
